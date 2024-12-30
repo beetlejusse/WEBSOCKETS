f44667088742cd7085c0cd6f1a01979d946dbd90
beetlejusse <vishalbelwal52@gmail.com>
2024-12-30 16:49:58 +0530

    authorization routes

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { loginController, logoutController, signUpController } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/signup", signUpController);
+
+router.post("/login", loginController);
+
+router.post("/logout", logoutController);
+
+export default router;
