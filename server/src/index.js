eb6197fbcf89af82b9817201ef1c918a5ae71719
beetlejusse <vishalbelwal52@gmail.com>
2025-01-16 21:50:45 +0530

    add CORS middleware to enable cross-origin requests

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from './routes/message.routes.js';
 import dotenv from 'dotenv';
 import connectDB from './db/index.db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,10 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 const port = process.env.PORT;
 
