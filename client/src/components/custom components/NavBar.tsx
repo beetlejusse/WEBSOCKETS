ba8317460d9d058010678ccc999b23f367ea5139
beetlejusse <vishalbelwal52@gmail.com>
2025-01-29 23:48:21 +0530

    feat(NavBar): update NavBar styles and improve user profile and logout button accessibility

--- client/src/components/custom components/NavBar.tsx
+++ client/src/components/custom components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAuthStore } from "@/store/useAuthStore";
+import { useAuthStore } from "../../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
@@ -7,59 +7,44 @@ const Navbar = () => {
 
   return (
     <header
-      className=" fixed w-full top-0 z-40 
-    backdrop-blur-lg backdrop-filter bg-opacity-95 shadow-lg"
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
     >
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link
-              to="/"
-              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
-            >
-              <div className="size-9 rounded-lg bg-white/20 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-white" />
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-extrabold text-white tracking-wide">
-                CHAT-A WEBSOCKET APP
-              </h1>
+              <h1 className="text-lg font-bold">Chat-A WEBSOCKET APP</h1>
             </Link>
           </div>
-          <div className="flex items-center gap-4">
+
+          <div className="flex items-center gap-2">
             <Link
               to={"/settings"}
               className={`
-              btn btn-sm btn-primary gap-2 transition-transform transform hover:scale-105
+              btn btn-sm gap-2 transition-colors
+              
               `}
-              title="Adjust your settings"
             >
               <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
             </Link>
 
-            {authUser ? (
+            {authUser && (
               <>
-                <div className="relative group">
-                  <Link to={"/inspectProfile"} className="hidden sm:inline">
-                    <button
-                      className="btn btn-sm btn-accent gap-2 transition-transform transform hover:scale-105"
-                      title="View Profile"
-                    >
-                      <User className="w-4 h-4" /> Profile
-                    </button>
-                  </Link>
-                </div>
-                <button
-                  className="btn btn-sm btn-error gap-2 transition-transform transform hover:scale-105"
-                  onClick={logout}
-                  title="Logout"
-                >
-                  <LogOut className="w-4 h-4" />
+                <Link to={"/inspectProfile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
               </>
-            ) : (
-              ""
             )}
           </div>
         </div>
@@ -67,5 +52,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
